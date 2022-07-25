952b2bd2af695dc45b309cfe07448e95c4a1b0fd
Wojtek Klimowicz <Wojciech.Klimowicz@education.gov.uk>
2022-07-25 11:18:41 +0100

    Fixed cli message

--- R/cli.R
+++ R/cli.R
@@ -3,7 +3,7 @@ cli_converting_complete <- function(output_directory) {
 
   cli::cli_alert_success("Converting complete")
 
-  cli::cli_alert_info("Run compiling on new folder with {.emph usoc_compile('{output_directory}')}\n")
+  cli::cli_alert_info("Run compiling on new folder with {.emph usoc <- usoc_compile('{output_directory}')}\n")
 
   cli::cli_end()
 }
