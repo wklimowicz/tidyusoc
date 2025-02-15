615ab57f0b54f6bf5825d3e40af1707efd2544c5
Wojtek Klimowicz <wojtekklimowicz96@gmail.com>
2025-02-15 08:01:36 +0000

    Update DESCRIPTION

--- inst/development/compile_usoc.R
+++ inst/development/compile_usoc.R
@@ -1,50 +1,50 @@
 library(data.table)
 library(tictoc)
 library(haven)
-setwd(here::here())
 devtools::load_all()
 library(tidyverse)
 
 
-usoc_convert("../data_usoc_raw/spss25",
-             "../data_usoc_rds/",
-incremental = TRUE,
-filter_files = "indresp"
+usoc_convert(
+  usoc_directory = "../data_usoc_raw/spss25",
+  new_directory = "../data_usoc_rds/",
+  incremental = FALSE,
+  # filter_files = "indresp"
 )
 
 user_extra_mappings <- function(usoc_file_column_names) {
+  # "scsf1", "scsf1", "factor",
+  # "sf1", "sf1", "factor",
+  # "bmi_dv", "bmi_dv", "numeric",
+  # "j1soc00_cc", "j1soc00_cc", "factor",
+  # "maedqf", "maedqf", "factor",
+  # "masoc00_cc", "masoc00_cc", "factor",
+  # "paedqf", "paedqf", "factor",
+  # "pasoc00_cc", "pasoc00_cc", "factor",
 
-    # "scsf1", "scsf1", "factor",
-    # "sf1", "sf1", "factor",
-    # "bmi_dv", "bmi_dv", "numeric",
-    # "j1soc00_cc", "j1soc00_cc", "factor",
-    # "maedqf", "maedqf", "factor",
-    # "masoc00_cc", "masoc00_cc", "factor",
-    # "paedqf", "paedqf", "factor",
-    # "pasoc00_cc", "pasoc00_cc", "factor",
-
-    life_sat <- pick_var(c("sclfsato", "lfsato"), usoc_file_column_names)
+  life_sat <- pick_var(c("sclfsato", "lfsato"), usoc_file_column_names)
 
   custom_variables <- tibble::tribble(
-    ~usoc_name,       ~new_name,     ~type,
+    ~usoc_name, ~new_name, ~type,
     "nchild_dv", "nchild_dv", "numeric",
     "ch1by4", "ch1by4", "factor",
     "basrate", "basrate", "numeric",
-    "ccare",     "ccare",    "factor",
-    "mlrnot3",   "mlrnot3",  "factor",
-    "xpchc",     "xpchc",    "factor",
-    "jbft_dv",   "jbft_dv",  "factor", #  Full  time  or  part  time
-    "jbsoc10_cc",   "jbsoc10_cc",  "factor",
+    "ccare", "ccare", "factor",
+    "mlrnot3", "mlrnot3", "factor",
+    "xpchc", "xpchc", "factor",
+    "jbft_dv", "jbft_dv", "factor", #  Full  time  or  part  time
+    "jbsoc10_cc", "jbsoc10_cc", "factor",
     life_sat, "lifesat", "factor"
-    )
+  )
 
   return(custom_variables)
 }
 
 indresp <- usoc_compile("../data_usoc_rds/",
-                     extra_mappings = user_extra_mappings,
-                     save_to_folder = TRUE,
-                     file = "indresp")
+  extra_mappings = user_extra_mappings,
+  save_to_folder = TRUE,
+  file = "indresp"
+)
 
 indresp[is.na(age), .N, waveid]
 indresp[is.na(sex), .N, waveid]
@@ -72,20 +72,19 @@ indresp[is.na(race), .N, waveid]
 # HHRESP --------------------------------
 
 user_extra_mappings <- function(usoc_file_column_names) {
-
-
   custom_variables <- tibble::tribble(
-    ~usoc_name,       ~new_name,     ~type,
+    ~usoc_name, ~new_name, ~type,
     "intdated", "intdated", "character",
-    )
+  )
 
   return(custom_variables)
 }
 
 hhresp <- usoc_compile("../data_usoc_rds/",
-                     extra_mappings = user_extra_mappings,
-                     save_to_folder = TRUE,
-                     file = "hhresp")
+  extra_mappings = user_extra_mappings,
+  save_to_folder = TRUE,
+  file = "hhresp"
+)
 
 
 
@@ -98,19 +97,18 @@ hhresp <- usoc_compile("../data_usoc_rds/",
 
 
 usoc <- usoc_compile("../data_usoc_rds/",
-                     file = "indresp")
+  file = "indresp"
+)
 
 
 
 youth_extra_mappings <- function(usoc_file_column_names) {
-
   custom_variables <- tibble::tribble(
-    ~usoc_name,       ~new_name,     ~type,
+    ~usoc_name, ~new_name, ~type,
     "yptcha", "yptcha", "factor"
-    )
+  )
 
   return(custom_variables)
-
 }
 
 usoc <- usoc_compile("../data_usoc_rds/", extra_mappings = youth_extra_mappings, file = "youth")
@@ -123,7 +121,7 @@ usoc[is.na(race), .N, wave]
 s <- readRDS("rds/bhps/ba_indresp.Rds")
 
 s %>%
-    tibble()
+  tibble()
 
 s$ba_race
 
@@ -136,12 +134,14 @@ usoc[, .N, ]
 
 usoc[, .N, sex]
 
-usoc[, sex := fcase(sex == "female", "Female",
-                    sex == "male", "Male",
-                    sex == "proxy", "proxy",
-                    rep(TRUE, nrow(usoc)), as.character(usoc$sex)) # Hack to recycle fcase
-                     |> as.factor()]
-                    
+usoc[, sex := fcase(
+  sex == "female", "Female",
+  sex == "male", "Male",
+  sex == "proxy", "proxy",
+  rep(TRUE, nrow(usoc)), as.character(usoc$sex)
+) # Hack to recycle fcase
+|> as.factor()]
+
 usoc[sex == "proxy", .N, wave]
 
 s <- readRDS("rds/bhps/bp_indresp.Rds")
@@ -153,17 +153,19 @@ s$bp_sex
 
 
 usoc[, .N, .(sclfsat2, x = jbsoc00_cc %ilike% "teach")] |>
-    ggplot() +
-    geom_col(aes(x = N, y = sclfsat2, fill = x), position = position_dodge())
+  ggplot() +
+  geom_col(aes(x = N, y = sclfsat2, fill = x), position = position_dodge())
 
 
 
 s <- readRDS("rds/bhps/bb_indresp.Rds")
 
-s %>% head(20)  %>% sie()
+s %>%
+  head(20) %>%
+  sie()
 
 
-usoc[, number_of_waves_present := .N  , by = pidp]
+usoc[, number_of_waves_present := .N, by = pidp]
 s <- readRDS("rds/bhps/bb_indresp.Rds")
 s$bb_indin91_lw
 
@@ -176,7 +178,10 @@ p$a_indscus_xw
 x <- readRDS("rds/bhps/xwaveid_bh.Rds")
 
 
-x |> as_factor() |> as.character() |> as.numeric()
+x |>
+  as_factor() |>
+  as.character() |>
+  as.numeric()
 
 usoc[pidp == 68035365]
 usoc[pidp == 68058485]
@@ -193,11 +198,3 @@ data.table::setnames(soc_coding, c("jbsoc00_cc", "occ"))
 soc_coding[, occ := factor(occ)]
 
 df <- merge(df, soc_coding, by = "jbsoc00_cc")
-
-
-
-
-
-
-
-
